9cac061891919707f8e0e680f0281cddf3678aed
Antellect <liljehorn.david@gmail.com>
2023-04-27 22:24:15 -0500

    added css.html

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
 
@@ -7,4 +8,11 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def css(request):
+    markdowner=Markdown()
+    return render(request,"encyclopedia/css.html", {
+        "css_content": markdowner.convert(util.get_entry('CSS'))
+    })
+
 
